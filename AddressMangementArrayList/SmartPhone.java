2b8a5e979dc0ca55b2defee00b22893864d68f75
JWHan <96328871+joonwonh@users.noreply.github.com>
2023-11-03 17:33:19 +0900

    Update SmartPhone.java

--- AddressMangementArrayList/SmartPhone.java
+++ AddressMangementArrayList/SmartPhone.java
@@ -1,92 +1,112 @@
 package AddressMangementArrayList;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class SmartPhone {
 	Scanner sc = new Scanner(System.in);
-	List<Address> addr = new ArrayList<>(10);
+	Map<String, Address>addr = new HashMap<>();
 	
 	public Address inputAddress()	{
-		System.out.print("이름 : ");
-		String name = sc.nextLine();
-
-		System.out.print("전화번호 : ");
-		String phoneNo = sc.nextLine();
 		
-		System.out.print("메일 : ");
-		String mail = sc.nextLine();
-		
-		System.out.print("주소 : ");
-		String address = sc.nextLine();
-		
-		System.out.print("그룹 : ");
-		String group = sc.nextLine();
-		
-		
-		return new Address(name, phoneNo, mail, address, group);
+		while(true)	{
+			System.out.print("전화번호 : ");
+			String phoneNo = sc.nextLine();
+			boolean result = addr.containsKey(phoneNo);
+			if(result == true)	{
+				System.out.println("중복된 전화번호입니다.");
+				break;
+			}
+			
+			System.out.print("이름 : ");
+			String name = sc.nextLine();
+			
+			System.out.print("ID : ");
+			String id = sc.nextLine();
+			
+			System.out.print("주소 : ");
+			String address = sc.nextLine();
+			
+			System.out.print("그룹 : ");
+			String group = sc.nextLine();
+			System.out.println("저장이 완료되었습니다.");
+			return addr.put(phoneNo, new Address(name, id, address, group));
 	}
-	
-	public void saveAddress(Address inputAddress)	{
-		addr.add(inputAddress);
-		System.out.println("데이터가 저장되었습니다. (" + addr.size() + ")");
+		return null;
 	}
 	
 	public void printAddress(Address data)	{
 		System.out.println("이름 : " + data.getName());
-		System.out.println("전화번호 : " + data.getPhoneNo());
-		System.out.println("메일 : " + data.getMail());
+		System.out.println("ID : " + data.getId());
 		System.out.println("주소 : " + data.getAddress());
 		System.out.println("그룹 : " + data.getGroup());
 		System.out.println("--------------------------------");
 		
-	}
+		}
 	
 	public void printAllAddress()	{
-		for(int i=0; i<addr.size(); i++)	{
-			printAddress(addr.get(i));
+		Set<String>keySet = addr.keySet();
+		Iterator<String>keyIterator = keySet.iterator();
+		while(keyIterator.hasNext())		{
+			String key = keyIterator.next();
+			Address value = addr.get(key);
+			System.out.println("전화번호 : " + key);
+			printAddress(value);
 		}
 	}
 	
-	public void searchAddress(String name)	{
-		for(int i=0; i<addr.size(); i++) {
-			if(addr.get(i).getName().equals(name))	{
-				printAddress(addr.get(i));
-				return;
+	public void searchAddressV(String phoneNo)	{
+		boolean result = addr.containsKey(phoneNo);
+		if(result == true)	{
+		Address value = addr.get(phoneNo);
+		printAddress(value);
 			}
+		else	{
+			System.out.println("연락처가 존재하지 않습니다.");
 		}
-		System.out.println("검색 결과가 없습니다.");
 	}
 
-	public void deleteAddress(String name)	{
-		for(int i=0; i<addr.size(); i++)	{
-			if(addr.get(i).getName().equals(name))		{
-				addr.remove(i);
-				System.out.println("삭제되었습니다.");
-				return;
-			}
+	public void deleteAddress(String phoneNo)	{
+		boolean result = addr.containsKey(phoneNo);
+		if(result == true)	{
+			addr.remove(phoneNo);
+			System.out.println("삭제가 완료되었습니다.");
 		}
-		System.out.println("다시 입력하세요.");
+		else	{
+			System.out.println("연락처가 존재하지 않습니다.");
+		}
 	}
 	
-	public void modifyAddress(String name, Address inputAddr)	{
-		for(int i=0; i<addr.size(); i++)	{
-			if(addr.get(i).getName().equals(name))		{
-					addr.set(i, inputAddr);
-					System.out.println("수정되었습니다.");
-			}
+	public void modifyAddress(String phoneNo)	{
+		boolean result = addr.containsKey(phoneNo);
+		if(result == true)	{
+			addr.remove(phoneNo);
+			inputAddress();
+			System.out.println("수정이 완료되었습니다.");
+		}
+		else	{
+			System.out.println("연락처가 존재하지 않습니다.");
 		}
 	}
 	public void printMenu() {
-		System.out.println("--------------------------------");
-		System.out.println("1. 연락처 등록");
-		System.out.println("2. 모든 연락처 출력");
-		System.out.println("3. 연락처 검색");
-		System.out.println("4. 연락처 삭제");
-		System.out.println("5. 연락처 수정");
-		System.out.println("6. 프로그램 종료");
-		System.out.println("--------------------------------");		
+		System.out.println();
+		System.out.println("								--------------------------------");
+		System.out.println("								|							|");
+		System.out.println("								|	주소록을 관리 해보자!	|");
+		System.out.println("								|							|");
+		System.out.println("								--------------------------------");
+		System.out.println("					---------------------------------------------------------");
+		System.out.println("					ㅣ 		   		1. 연락처 등록					ㅣ");
+		System.out.println("					ㅣ 		   		2. 모든 연락처 출력				ㅣ");
+		System.out.println("					ㅣ 		   		3. 연락처 검색					ㅣ");
+		System.out.println("					ㅣ 		   		4. 연락처 삭제					ㅣ");
+		System.out.println("					ㅣ 		   		5. 연락처 수정					ㅣ");
+		System.out.println("					ㅣ 		   		6. 프로그램 종료					ㅣ");
+		System.out.println("					---------------------------------------------------------");
+		System.out.println();
 	}
 
 }
